7dc3afdb1779db194ec7d24d03baa44535157279
Mohammed Maaz Rayeen <mmaaz0510@gmail.com>
2025-04-01 00:27:50 +0530

    Update product.route.js

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,18 +1,28 @@
 import express from "express";
-import { createProduct, deleteProduct, getRecommendedProducts, getProductsByCategory, toggleFeaturedProduct, getAllProducts, getFeaturedProducts} from "../controllers/product.controller.js";
-import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
+import {
+    createProduct,
+    deleteProduct,
+    getAllProducts,
+    getFeaturedProducts,
+    getProductsByCategory,
+    getRecommendedProducts,
+    toggleFeaturedProduct,
+} from "../controllers/product.controller.js";
+import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
-router.get("/recommendations", getRecommendedProducts);
 router.get("/category/:category", getProductsByCategory);
+router.get("/recommendations", getRecommendedProducts);
 router.post("/", protectRoute, adminRoute, createProduct);
-// patch is used when we want to update some fields and put is used when we want to update all fields.
-router.patch("/:id", protectRoute, adminRoute, toggleFeaturedProduct );
+router.patch("/:id", protectRoute, adminRoute, toggleFeaturedProduct);
 router.delete("/:id", protectRoute, adminRoute, deleteProduct);
 
-
 export default router;
+
+
+
+
+
