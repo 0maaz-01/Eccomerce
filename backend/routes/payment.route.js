41974316a84b638287b54b3e3b0433f172f0c001
Mohammed Maaz Rayeen <mmaaz0510@gmail.com>
2025-04-02 23:14:23 +0530

    Update payment.route.js

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,24 +1,10 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { createCheckoutSession } from "../controllers/payment.controller.js";
+import { checkoutSuccess, createCheckoutSession } from "../controllers/payment.controller.js";
 
 const router = express.Router();
 
-
-router.post("/create-checkout-session", protectRoute, async (req, res) => {
-    try {
-        const { products, couponCode } = req.body;
-
-        if (!Array.isArray(products) || products.length == 0) {
-            return res.status(400).json({ error: "Invalud or empty products array." });
-        }
-    }
-
-    catch (error) {
-
-    }
-});
-
-
+router.post("/create-checkout-session", protectRoute, createCheckoutSession);
+router.post("/checkout-success", protectRoute, checkoutSuccess);
 
 export default router;
