4e832ef6f331d1b02e04729fdec301fed7601775
Mohammed Maaz Rayeen <mmaaz0510@gmail.com>
2025-04-03 13:55:52 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,12 @@
-// lucide react contains icons.
 import { ShoppingCart, UserPlus, LogIn, LogOut, Lock } from "lucide-react";
 import { Link } from "react-router-dom";
 import { useUserStore } from "../stores/useUserStore";
-// import { useCartStore } from "../stores/useCartStore";
+import { useCartStore } from "../stores/useCartStore";
 
 const Navbar = () => {
-
 	const { user, logout } = useUserStore();
 	const isAdmin = user?.role === "admin";
-	// const { cart } = useCartStore();
+	const { cart } = useCartStore();
 
 	return (
 		<header className='fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800'>
@@ -18,56 +16,47 @@ const Navbar = () => {
 						E-Commerce
 					</Link>
 
-
 					<nav className='flex flex-wrap items-center gap-4'>
 						<Link
 							to={"/"}
 							className='text-gray-300 hover:text-emerald-400 transition duration-300
-					 		ease-in-out'>
+					 ease-in-out'
+						>
 							Home
 						</Link>
-
-
-							{/*If the user is active then only we can take him to thr cart page.*/}
-							{user && (
-								<Link
-									to={"/cart"}
-									className='relative group text-gray-300 hover:text-emerald-400 transition duration-300 ease-in-out'>	
-
-									{/*Adding the shopping cart icon to the button*/}
-									<ShoppingCart className='inline-block mr-1 group-hover:text-emerald-400' size={20} />
-									<span className='hidden sm:inline'>Cart</span>
-									
-									{/*If there are elements in the cart then we will display the number of elements in the cart along with the cart symbol in the button.*/}
-									{/*{cart.length > 0 && ( */}
-										<span
-											className='absolute -top-2 -left-2 bg-emerald-500 text-white rounded-full px-2 py-0.5 
-										text-xs group-hover:bg-emerald-400 transition duration-300 ease-in-out'
-										>
-											3 {/*{cart.length}*/}
-										</span>
-									{/*})}*/}
-								</Link>
-							)}
-
-
+						{user && (
+							<Link
+								to={"/cart"}
+								className='relative group text-gray-300 hover:text-emerald-400 transition duration-300 
+							ease-in-out'
+							>
+								<ShoppingCart className='inline-block mr-1 group-hover:text-emerald-400' size={20} />
+								<span className='hidden sm:inline'>Cart</span>
+								{cart.length > 0 && (
+									<span
+										className='absolute -top-2 -left-2 bg-emerald-500 text-white rounded-full px-2 py-0.5 
+									text-xs group-hover:bg-emerald-400 transition duration-300 ease-in-out'
+									>
+										{cart.length}
+									</span>
+								)}
+							</Link>
+						)}
 						{isAdmin && (
 							<Link
 								className='bg-emerald-700 hover:bg-emerald-600 text-white px-3 py-1 rounded-md font-medium
 								 transition duration-300 ease-in-out flex items-center'
-								to={"/secret-dashboard"}>
-								
-								{/*Lock is an icon from the lucide library.*/}
+								to={"/secret-dashboard"}
+							>
 								<Lock className='inline-block mr-1' size={18} />
 								<span className='hidden sm:inline'>Dashboard</span>
 							</Link>
 						)}
 
-
 						{user ? (
 							<button
 								className='bg-gray-700 hover:bg-gray-600 text-white py-2 px-4 
-								rounded-md flex items-center transition duration-300 ease-in-out'
+						rounded-md flex items-center transition duration-300 ease-in-out'
 								onClick={logout}
 							>
 								<LogOut size={18} />
@@ -83,7 +72,6 @@ const Navbar = () => {
 									<UserPlus className='mr-2' size={18} />
 									Sign Up
 								</Link>
-
 								<Link
 									to={"/login"}
 									className='bg-gray-700 hover:bg-gray-600 text-white py-2 px-4 
