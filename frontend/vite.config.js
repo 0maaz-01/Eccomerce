76a3328c36591d3cc8a4f038fc5fc146d05a22c5
Mohammed Maaz Rayeen <mmaaz0510@gmail.com>
2025-04-03 13:47:43 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	server: {
+    // when a request is made to the /api path in the frontend, Vite will forward that request to http://localhost:5000.
+		proxy: {
+			"/api": {
+				target: "http://localhost:5000",
+			},
+		},
+	},
+});
